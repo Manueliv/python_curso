bc396385b77d1896b6dab4781471804058debacf
unknown <manueliv2008@hotmail.com>
2025-11-16 20:56:57 -0600

    actulizando

--- Dia 2/Proyecto2.py
+++ Dia 2/Proyecto2.py
@@ -1,8 +1,8 @@
 nombre = input("Ingrese su nombre: ")
-ventas = input("Ingrese sus ventas : ")
+ventas = input("Ingrese sus ventas : ") # aqui se pudo haver indicado directamente que lo se ingrese en el input sea otro tipo de dato ejem: ventas = int(input("")
 
 venta_convertida_float = float(ventas) #Hacemos la conversion del imput de ventas a float
 
-comision = venta_convertida_float * 13 /100 #guardamos el resultado en la varible comision
+comision = venta_convertida_float * 13 /100 #guardamos el resultado en la varible comision, (tambien aqui se pudo haber indicado round para aplicar a la varible  )
 comision_redondeada = round(comision, 2) #ahora ese resultado lo redondeamos indicando como maximo 2 decimales y guradmos en la variable
 print(f"Hola {nombre}, este mes ganaste ${comision_redondeada} en comisiones por ventas \"Felicidades\"")
